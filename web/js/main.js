df32605468a7c3656722e01b6daa7ce0cb5c1498
Lata <bvlata@mail.ru>
2020-11-09 14:15:58 +0500

    Update and rename bootstrap.min.js to main.js

---
+++ web/js/main.js
@@ -0,0 +1,24 @@
+ window.onscroll = function () {
+                scrollFunction()
+            };
+
+            function scrollFunction() {
+                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                    document.getElementById("movetop").style.display = "block";
+                } else {
+                    document.getElementById("movetop").style.display = "none";
+                }
+            }
+
+            // When the user clicks on the button, scroll to the top of the document
+            function topFunction() {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            }
+
+
+             $(function () {
+            $('.navbar-toggler').click(function () {
+                $('body').toggleClass('noscroll');
+            })
+        });
