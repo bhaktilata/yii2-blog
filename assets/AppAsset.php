4ea9a3488d1b53e6d73317aa42333fd3cb7a9397
Lata <bvlata@mail.ru>
2020-11-09 13:29:03 +0500

    Update AppAsset.php

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,21 +20,24 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        //'web/css/bootstrap.min.css',
-        //'css/bootstrap.css',
-        'css/style.css',
-        //'css/animate.min.css',
-                
+
+        'css/style-starter.css',
+        'css/custom.css',
+        '//fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap',
+        '//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap',
+        //'css/site.css',
     ];
     public $js = [
-        //'js/responsiveslides.min.js',
-        // 'js/jquery.js',
-        //'js/bootstrap.js',
-        'js/jquery.flexisel.js',
-        //'js/jquery-1.11.1.min.js',
-        ];
+
+        'js/theme-change.js',
+        'js/jquery-3.3.1.min.js',
+        'js/bootstrap.min.js',
+        'js/main.js',
+    ];
     public $depends = [
+
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
+        //'yii\bootstrap\BootstrapAsset',
+
     ];
 }
