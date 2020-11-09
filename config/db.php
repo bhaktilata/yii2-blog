ef412f35a77aadff8824934597d12cca690fb83e
Lata <bvlata@mail.ru>
2020-11-09 13:28:07 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=baking',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-
+    'tablePrefix' => 'wedf_',
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
